167060391ef94ee8dd9d5db66dc52f6303a45104
Maciej Milewski <macie.milewski@gmail.com>
2026-07-31 17:53:23 +0200

    EtherType and IpProtocol enums

---
+++ src/PacketBuilder.Core/Protocols/EtherType.cs
@@ -0,0 +1,9 @@
+﻿namespace PacketBuilder.Core.Protocols;
+
+/// <summary>EtherType values (Ethernet II header, offset 12).</summary>
+public enum EtherType : ushort
+{
+    Ipv4 = 0x0800,
+    Arp = 0x0806,
+    Ipv6 = 0x86DD,
+}
---
+++ src/PacketBuilder.Core/Protocols/IpProtocol.cs
@@ -0,0 +1,9 @@
+﻿namespace PacketBuilder.Core.Protocols;
+
+/// <summary>IANA IP protocol numbers (IPv4 header, offset 9).</summary>
+public enum IpProtocol : byte
+{
+    Icmp = 1,
+    Tcp = 6,
+    Udp = 17,
+}
