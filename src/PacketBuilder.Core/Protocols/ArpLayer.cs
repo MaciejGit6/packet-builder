5c142f5314ede3477a93697ee181f3552f44b896
Maciej Milewski <macie.milewski@gmail.com>
2026-08-02 15:26:56 +0200

    Ipv4Address and ArpLayer

---
+++ src/PacketBuilder.Core/Protocols/ArpLayer.cs
@@ -0,0 +1,32 @@
+﻿using PacketBuilder.Core.Buffers;
+
+namespace PacketBuilder.Core.Protocols;
+
+public enum ArpOperation : ushort
+{
+    Request = 1,
+    Reply = 2,
+}
+
+/// <summary>An ARP-over-Ethernet message for IPv4 (28 bytes).</summary>
+public sealed class ArpLayer : ILayer
+{
+    public ArpOperation Operation { get; init; } = ArpOperation.Request;
+    public required MacAddress SenderMac { get; init; }
+    public required Ipv4Address SenderIp { get; init; }
+    public MacAddress TargetMac { get; init; }          // left as 0 for a request
+    public required Ipv4Address TargetIp { get; init; }
+
+    public void Write(ref PacketWriter writer)
+    {
+        writer.WriteUInt16(1);                        // hardware type: Ethernet
+        writer.WriteUInt16((ushort)EtherType.Ipv4);  //protocol type: IPv4
+        writer.WriteUInt8(6);                         //hardware address length
+        writer.WriteUInt8(4);                         // protocol address length
+        writer.WriteUInt16((ushort)Operation);
+        SenderMac.WriteTo(writer.Reserve(6));
+        SenderIp.WriteTo(writer.Reserve(4));
+        TargetMac.WriteTo(writer.Reserve(6));
+        TargetIp.WriteTo(writer.Reserve(4));
+    }
+}
---
+++ src/PacketBuilder.Core/Protocols/Ipv4Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacketBuilder.Core.Protocols;
+
+/// <summary>
+/// A 32bit Ipv4 address
+/// </summary>
+
+
+public readonly struct Ipv4Address
+{
+    private readonly byte _a, _b, _c, _d;
+
+    public Ipv4Address(byte a, byte b, byte c, byte d) => (_a, _b, _c, _d) = (a, b, c, d);
+
+    public void WriteTo(Span<byte> destination)
+    {
+        destination[0] = _a; destination[1] = _b; destination[2] = _c; destination[3] = _d;
+    }
+
+    public static Ipv4Address Parse(string text)
+    {
+        var p = text.Split('.');
+        if (p.Length != 4)
+            throw new FormatException($"'{text}' is not an Ipv4 address.");
+        return new Ipv4Address(byte.Parse(p[0]), byte.Parse(p[1]), byte.Parse(p[2]), byte.Parse(p[3]));
+    }
+    public override string ToString() => $"{_a}.{_b}.{_c}.{_d}";
+    
+}
